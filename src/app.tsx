1a0043c4d0c12fa783a7eb4c929daf5b0645eb5f
Daniel Levi <daniellevi000@gmail.com>
2024-09-11 14:02:04 -0300

    wip: integratiosn data

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,44 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { EmptyGoals } from './components/empty-gaols'
+import { useQuery } from '@tanstack/react-query'
+import { WeeklySummary } from './components/summary'
+import { getSummary } from './http/get-summary'
+import { Loader2 } from 'lucide-react'
 
+type summaryType = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
 export function App() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
+
+  if (isLoading || !data) {
+    return (
+      <div className="h-screen flex items-center justify-center">
+        <Loader2 className="text-zinc-500 animate-spin size-10" />
+      </div>
+    )
+  }
   return (
     <Dialog>
-      <EmptyGoals />
+      {data.summary.total > 0 ? (
+        <WeeklySummary summary={data.summary} />
+      ) : (
+        <EmptyGoals />
+      )}
+
       <CreateGoal />
     </Dialog>
   )
